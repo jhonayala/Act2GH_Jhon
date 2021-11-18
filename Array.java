94f6ce2dba1b550f1e48f4a0df637649cd67df7f
Jhon <a_jhondeivis.ayala.braithwite@iespablopicasso.es>
2021-11-18 14:28:40 +0100

    añado imprimir por pantalla al array.java

--- Array.java
+++ Array.java
@@ -3,15 +3,15 @@ package tareas_git;
 public class Array {
 
 	public static void main(String[] args) {
-		
-		int [] array=new int[20];
-		
+
+		int[] array = new int[20];
+
 		for (int i = 0; i < array.length; i++) {
-			int m=(int)(Math.random()*10);
-			array[i]=m;
-			
+			int m = (int) (Math.random() * 10);
+			array[i] = m;
+			System.out.println(array[i] + " ");
+
 		}
-		
 
 	}
 
